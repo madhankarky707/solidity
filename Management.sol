4f05e57e89925b8144c61b4f94b8f8c1241920e2
Madhan kumar <36563503+madhankarky707@users.noreply.github.com>
2018-02-21 13:13:10 +0530

    Update and rename bank wallet.sol to Management.sol

--- Management.sol
+++ Management.sol
@@ -1,3 +1,6 @@
+program:1
+BANK WALLET
+
 pragma solidity^0.4.0;
 contract bank
 {
@@ -49,4 +52,48 @@ contract bank
     {
         owner=owner1;
     }
+}
+
+
+PROGRAM:2
+STUDENT INTERVIEW APLLY
+
+contract student
+{
+     struct student
+     {
+     string name;
+     uint256 age;
+     uint256 id;
+     }
+     mapping(address => student)student1;
+     mapping(address => student)student2;
+   
+     function olddata(string _name,uint256 _age,uint256 _id,address key1)public 
+     {
+         student1[key1].name=_name;
+         student1[key1].age=_age;
+         student1[key1].id=_id;
+     }
+     function newdata(string _name,uint256 _age,uint256 _id,address key2)public 
+     {
+         student2[key2].name=_name;
+         student2[key2].age=_age;
+         student2[key2].id=_id;
+         
+     }
+     function output()public view returns(string)
+     {
+         for(uint256 i=0;i<3;i++)
+         {
+             if(student1[i].id=_id==student2[key2].id=_id)
+             {
+                 return valid;
+             }
+             else
+             {
+                 return invalid;
+             }
+         }
+     }
 }
