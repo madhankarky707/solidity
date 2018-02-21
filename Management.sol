519e0104bb12ec958475495fb66e1f8792e9984e
Madhan kumar <36563503+madhankarky707@users.noreply.github.com>
2018-02-21 14:09:58 +0530

    Update Management.sol

--- Management.sol
+++ Management.sol
@@ -97,3 +97,34 @@ contract student
          }
      }
 }
+
+
+PROGRAM:3
+HOTEL
+
+pragma solidity ^0.4.0;
+contract hotelmanagement
+{
+    struct dish1{
+        string dishname;
+        uint256 dishprice;
+    }
+    mapping(address=>dish1)dish;
+   // mapping(address=>dish1)customer1;
+    function todayspl(address a,string _name,uint256 _price)public
+    {
+        dish[a].dishname=_name;
+        dish[a].dishprice=_price;
+    }
+    modifier check(address a,uint256 price)
+    {
+        require(dish[a].dishprice==price);
+        _;
+    }
+    function customer(address a,string name,uint256 price)check(a,price)public constant returns(string)
+    {
+       
+          return "token";
+    }
+    
+}
