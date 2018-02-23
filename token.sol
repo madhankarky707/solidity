d6ecad77a902fc21d464f462fe017c8c0b92ea6d
Madhan kumar <36563503+madhankarky707@users.noreply.github.com>
2018-02-23 12:29:55 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,3 +1,6 @@
+     PROGRAM:1
+     ONE SPENDER:-
+
 contract erc20
 {
      function initialization(string _name,string _symbol)public /*constant returns(bool success)*/;
@@ -59,3 +62,92 @@ contract token is erc20
      return allowed[spender][msg.sender];
 }
 }
+
+
+
+PROGRAM:2
+     TWO SPENDER:-
+     
+     contract erc20
+{
+     function initialization(string _name,string _symbol)public;
+     function transfer(address to,uint256 value)public  returns(bool success);
+     function transferspender(address from,address to ,uint256 value)public returns(bool success);
+     function transferspdr(address spdr,address _spender,address to ,uint256 value)public returns(bool success);
+     function approvalspender(address spender,uint256 value)public returns(uint256);
+     function approvalspdr(address spender,address spdr,uint256 value)public;
+     function balance(address _owner) constant returns (uint256 balance);
+     function balancespender(address spender)public constant  returns (uint256 balance);
+     function balancespdr(address spdr,address n)public constant  returns (uint256 );
+     event Transfer(address from, address to, uint256 value);
+}
+contract token is erc20
+{
+    string public name;
+    string public symbol;
+    uint256 public decimal=18;
+    uint256 public totalsupply=1000;
+    
+     mapping(address=>uint256)balanceOf;
+     mapping (address => mapping (address => uint256)) allowed;
+     mapping(address=>mapping(address => uint256))allow;
+    function initialization(string _name,string _symbol)public //constant returns(bool success)
+{
+    balanceOf[msg.sender]=totalsupply;
+    name=_name;
+    symbol=_symbol;
+}
+   function transfer(address to,uint256 value)public returns(bool success)
+   {
+        require(balanceOf[msg.sender]>=value);
+       balanceOf[msg.sender]-=value;
+       balanceOf[to]+=value;
+   Transfer(msg.sender,to,value);
+       return true;
+   }
+   function transferspender(address from,address to ,uint256 value)public returns(bool success)
+   {
+     require(allowed[from][msg.sender]>=value);
+       {
+        allowed[from][msg.sender]-=value;
+       balanceOf[to]+=value;
+       }
+        Transfer(from,to,value);
+          return true;
+   }
+    function transferspdr(address spdr,address _spender,address to ,uint256 value)public returns(bool success)
+   {
+     require(allow[spdr][_spender]>=value);
+       {
+        allow[spdr][_spender]-=value;
+       balanceOf[to]+=value;
+       }
+        Transfer(spdr,to,value);
+          return true;
+   }
+   function balance(address _owner) constant returns (uint256 ) {
+     return balanceOf[_owner];
+      
+  }
+    
+   function approvalspender(address spender,uint256 value)public returns(uint256)
+    {
+        allowed[spender][msg.sender]=value;
+        balanceOf[msg.sender]-=value;
+        return allowed[spender][msg.sender];
+  
+    }
+    function approvalspdr(address spender,address spdr,uint256 value)public
+  {
+       allow[spdr][spender]=value;
+       allowed[spender][msg.sender]-=value;
+   }
+    function balancespender(address spender)public constant  returns (uint256 ) {
+     return allowed[spender][msg.sender];
+}
+function balancespdr(address spdr,address _spender)public constant  returns (uint256 ) {
+     return allow[spdr][_spender];
+}
+}
+
+
